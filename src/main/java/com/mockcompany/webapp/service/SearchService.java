f62d1a1330b76423ceb1e95e8494332493439e83
edukojumanikumar <manikumaredukoju@gmail.com>
2025-06-11 22:03:04 -0400

    Update SearchService.java

--- src/main/java/com/mockcompany/webapp/service/SearchService.java
+++ src/main/java/com/mockcompany/webapp/service/SearchService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Collections;
+
 
 @Service
 public class SearchService {
